16c5e744ea2257e41dc555c93fe3c7fa750776c0
kob616 <44599970+kob616@users.noreply.github.com>
2026-05-16 10:11:05 +0700

    Update board.mk

--- board.mk
+++ board.mk
@@ -2,12 +2,14 @@ include $(BOARD_DIR)/firmware/firmware.mk
 
 
 BOARDINC += $(BOARD_DIR)/generated/controllers/generated
+BOARDINC += $(BOARD_DIR)
 
 # defines SHORT_BOARD_NAME
 include $(BOARD_DIR)/meta-info.env
 
 # this would save some flash while being unable to update WBO controller firmware
 DDEFS += -DEFI_WIDEBAND_FIRMWARE_UPDATE=FALSE
+DDEFS += -DMCUCONF_H
 
 # assign critical LED to a non-existent pin if you do not have it on your board
 # good old PD14 is still the default value
@@ -19,7 +21,4 @@ DDEFS += -DEFI_WIDEBAND_FIRMWARE_UPDATE=FALSE
 
 #see main repo for details on this any many other optional subsystems. We have too many, one has to choose what fits into his choice of stm32
 #DDEFS += -DEFI_ONBOARD_MEMS=TRUE
-BOARDINC += .
-DDEFS += -DMCUCONF_H
 
-//DDEFS += -DSTM32_WDG_USE_IWDG=TRUE
