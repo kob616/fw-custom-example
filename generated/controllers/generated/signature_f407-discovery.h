6c298ee85542711534034fa74ee9a228a38444b3
GitHub git update Action <action@github.com>
2026-05-16 02:04:31 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_f407-discovery.h
+++ generated/controllers/generated/signature_f407-discovery.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2077432100
-#define TS_SIGNATURE "rusEFI main.2026.05.15.f407-discovery.2077432100"
+#define SIGNATURE_HASH 1564182503
+#define TS_SIGNATURE "rusEFI main.2026.05.16.f407-discovery.1564182503"
